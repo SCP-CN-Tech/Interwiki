cf3a8652c5c8d50e8cd383e8916992cd4bea2782
Sekai-s <51074335+Sekai-s@users.noreply.github.com>
2021-04-14 00:05:40 +0800

    Add files via upload

--- branches-info-wl.js
+++ branches-info-wl.js
@@ -1,30 +1,37 @@
 var IntWL = {
     "cn": {
-        "name": "中文",
-        "head": "其他语言",
-        "url": "http://scp-wiki-cn.wikidot.com/",
-        "id": "530812",
-        "category": "wanderers:"
+        name: "中文",
+        head: "其他语言",
+        url: "http://scp-wiki-cn.wikidot.com/",
+        id: "530812",
+        category: "wanderers:"
     },
     "en": {
-        "name": "English",
-        "head": "Languages",
-        "url": "http://wanderers-library.wikidot.com/",
-        "id": "146034",
-        "category": ""
+        name: "English",
+        head: "Languages",
+        url: "http://wanderers-library.wikidot.com/",
+        id: "146034",
+        category: ""
     },
     "jp": {
         name: "日本語",
         head: "他言語版",
         url: "http://scp-jp.wikidot.com/",
         id: "578002",
-        "category": "wanderers:"
+        category: "wanderers:"
     },
     "ko": {
         name: "한국어",
         head: "다른 언어",
         url: "http://wanderers-library-ko.wikidot.com/",
         id: "621849",
-        "category": ""
+        category: ""
+    },
+    "pl": {
+        name: "Polski",
+        head: "W innych językach",
+        url: "http://wanderers-library-pl.wikidot.com/",
+        id: "4593099",
+        category: ""
     }
 };
