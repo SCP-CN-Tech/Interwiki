be66e07486ab9d59183f69a4b2c76c7b19c036ba
Sekai-s <51074335+Sekai-s@users.noreply.github.com>
2021-05-07 10:25:09 +0800

    upgrade to HTTPs

--- branches-info-wl.js
+++ branches-info-wl.js
@@ -2,35 +2,35 @@ var IntWL = {
     "cn": {
         name: "中文",
         head: "其他语言",
-        url: "http://scp-wiki-cn.wikidot.com/",
+        url: "https://scp-wiki-cn.wikidot.com/",
         id: "530812",
         category: "wanderers"
     },
     "en": {
         name: "English",
         head: "Languages",
-        url: "http://wanderers-library.wikidot.com/",
+        url: "https://wanderers-library.wikidot.com/",
         id: "146034",
         category: ""
     },
     "jp": {
         name: "日本語",
         head: "他言語版",
-        url: "http://scp-jp.wikidot.com/",
+        url: "https://scp-jp.wikidot.com/",
         id: "578002",
         category: "wanderers"
     },
     "ko": {
         name: "한국어",
         head: "다른 언어",
-        url: "http://wanderers-library-ko.wikidot.com/",
+        url: "https://wanderers-library-ko.wikidot.com/",
         id: "621849",
         category: ""
     },
     "pl": {
         name: "Polski",
         head: "W innych językach",
-        url: "http://wanderers-library-pl.wikidot.com/",
+        url: "https://wanderers-library-pl.wikidot.com/",
         id: "4593099",
         category: ""
     }
--- createResizeIframe.js
+++ createResizeIframe.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var mySite = document.referrer;
-    mySite = mySite.split("http://").join("");
-    mySite = "http://" + mySite.split("/")[0] + "/";
-    
+    var mySite = document.referrer.replace(/(https?:\/\/[^/]*)\/?.*/, '$1/');
+
     var iframeSet = document.getElementById('iframeset');
     var oldHeight = 0;
 
@@ -12,22 +10,22 @@ document.addEventListener("DOMContentLoaded",function(){
     createResizeIframe();
 
     function createResizeIframe(){
-        
+
         var Height = getMyHeight();
         var iframe = document.createElement("iframe");
         var CacheBreak = String(Math.floor(Math.random() * 10000));
- 
+
         if(Height != oldHeight){
             iframeSet.innerHTML = '';
-            iframe.src = mySite + "common--javascript/resize-iframe.html?" + CacheBreak + "#" + Height +url;
+            iframe.src = mySite + "common--javascript/resize-iframe.html?" + CacheBreak + "#" + Height + url;
             iframe.style.display = "none";
- 
+
             iframeSet.appendChild(iframe);
             oldHeight = Height;
         }
         setTimeout(createResizeIframe,50);
    }
- 
+
     function getMyHeight() {
 
         return iframeSet.getBoundingClientRect().top;
