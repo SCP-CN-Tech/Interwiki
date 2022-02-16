7a4081a874b88a28837608c3ee4176b4324adaaa
Ross Williams <rossjrw@gmail.com>
2022-02-16 17:16:52 +0000

    Hide all PageLookupQModule specifics in function

--- js/links.js
+++ js/links.js
@@ -1,3 +1,4 @@
+/* global wikidotLookup */
 /* exported addTranslations */
 
 "use strict";
@@ -28,7 +29,7 @@
  * branches. Also sets the hover information in the refresh link to the
  * current time.
  *
- * @param {Object.<String, Branch>} branches - The branches configuration
+ * @param {Object.<string, Branch>} branches - The branches configuration
  * for the current community.
  * @param {String} currentBranchLang - The language code of the current
  * branch, as defined in the community's branches config.
@@ -56,92 +57,11 @@ function addTranslations(branches, currentBranchLang, pagename, resize) {
     // TODO Make the string here translatable
     "Click to refresh (Last refresh：" + new Date().toLocaleString() + ")";
 
-  // For all configured sites that are not the current site, request
-  // translation data about this page
-  Object.keys(branches).forEach(function (branchLang) {
-    if (branchLang === currentBranchLang) return;
-    var branch = branches[branchLang];
-    addTranslationForBranch(
-      currentBranch,
-      branchLang,
-      branch,
-      pagename,
-      resize
-    );
+  wikidotLookup(currentBranch, branches, pagename, function () {
+    addTranslationLink(arguments);
+    // Resize the iframe to account for the new link
+    resize();
   });
-}
-
-/**
- * For the given target branch, find a page that is a translation of the
- * paat fullname in the current branch. If one exists, create a menu item
- * for it.
- *
- * @param {Branch} currentBranch - Configuration for the current branch.
- * @param {String} targetBranchLang - The language code of the branch.
- * @param {Branch} targetBranch - Configuration for the branch to lookup.
- * @param {String} fullname - The Wikidot fullname of the page to lookup.
- * @param {Function} resize - A callback to resize the iframe after adding
- * a new translation.
- */
-function addTranslationForBranch(
-  currentBranch,
-  targetBranchLang,
-  targetBranch,
-  fullname,
-  resize
-) {
-  // Replace the current site's category with the target site's category,
-  // if either are defined
-  // E.g.:
-  // WL CN "wanderers:page" -> WL EN "page"
-  // WL EN "page" -> WL CN "wanderers:page"
-  var targetFullname = fullname.replace(
-    new RegExp("^" + currentBranch.category),
-    targetBranch.category
-  );
-
-  // A fullname can be at most 60 characters long. If the target fullname
-  // is any longer, truncate it
-  targetFullname = targetFullname.substring(0, 60).replace(/-$/, "");
-
-  // If the original fullname was 59 characters long (because the limit is
-  // 60, minus one if it would have ended with a hyphen), it could have
-  // been truncated. If the target fullname is shorter than the original
-  // fullname (due to stripping the category), the last bit of the fullname
-  // is not recoverable
-  var couldHaveBeenTruncated =
-    fullname.length >= 59 && targetFullname.length < fullname.length;
-
-  // Find pages in the target branch matching this fullname
-  findPagesInSiteStartingWith(
-    targetBranch.id,
-    targetFullname,
-    function (fullnames) {
-      // If there is an exact match, a translation has been found
-      if (
-        fullnames.some(function (matchedFullname) {
-          // If the end of the fullname is possibly missing, check only
-          // that the matched fullname starts with the target.
-          // This is unlikely to produce a false positive because the
-          // fullnames involved are very long (~60 chars)
-          if (couldHaveBeenTruncated) {
-            return matchedFullname.indexOf(targetFullname) === 0;
-          }
-          // Otherwise, check for exact matches only
-          return matchedFullname === targetFullname;
-        })
-      ) {
-        addTranslationLink(
-          targetBranch.url + targetFullname,
-          targetBranch.name,
-          targetBranchLang
-        );
-
-        // Resize the iframe to account for the new link
-        resize();
-      }
-    }
-  );
 }
 
 /**
--- js/lookup/wikidot.js
+++ js/lookup/wikidot.js
@@ -3,22 +3,108 @@
 "use strict";
 
 /**
+ * @callback addLinkCallback
+ * @param {String} pageUrl
+ * @param {String} branchName
+ * @param {String} branchLang
+ */
+
+/**
  * Searches for pages whose fullname matches the given string in the given
  * set of Wikidot sites using Wikidot's PageLookupQModule.
  *
- * @param {Object.<String, Branch>} branches - The branches configuration
+ * @param {Branch} currentBranch - Configuration for the current branch.
+ * @param {Object.<string, Branch>} branches - The branches configuration
  * for the current community. All passed branches will be searched for the
  * target page.
  * @param {String} fullname - The substring to compare fullnames against.
  * If an underscore "_" is provided, all pages on the site will match.
- * @param {findPagesCallback} callback
+ * @param {addLinkCallback} addLink - A function that will be
+ * called for each found translation.
  */
-function wikidotLookup(branches, fullname, callback) {}
+function wikidotLookup(currentBranch, branches, fullname, addLink) {
+  Object.keys(branches).forEach(function (branchLang) {
+    if (branches[branchLang].url === currentBranch.url) return;
+    var branch = branches[branchLang];
+    addTranslationForBranch(
+      currentBranch,
+      branchLang,
+      branch,
+      fullname,
+      addLink
+    );
+  });
+}
 
 /**
- * @callback findPagesCallback
- * @param {String[]} fullnames
+ * For the given target branch, find a page that is a translation of the
+ * paat fullname in the current branch. If one exists, create a menu item
+ * for it.
+ *
+ * @param {Branch} currentBranch - Configuration for the current branch.
+ * @param {String} targetBranchLang - The language code of the branch.
+ * @param {Branch} targetBranch - Configuration for the branch to lookup.
+ * @param {String} fullname - The Wikidot fullname of the page to lookup.
+ * @param {addLinkCallback} addLink - A function that will be
+ * called for each found translation.
  */
+function addTranslationForBranch(
+  currentBranch,
+  targetBranchLang,
+  targetBranch,
+  fullname,
+  addLink
+) {
+  // Replace the current site's category with the target site's category,
+  // if either are defined
+  // E.g.:
+  // WL CN "wanderers:page" -> WL EN "page"
+  // WL EN "page" -> WL CN "wanderers:page"
+  var targetFullname = fullname.replace(
+    new RegExp("^" + currentBranch.category),
+    targetBranch.category
+  );
+
+  // A fullname can be at most 60 characters long. If the target fullname
+  // is any longer, truncate it
+  targetFullname = targetFullname.substring(0, 60).replace(/-$/, "");
+
+  // If the original fullname was 59 characters long (because the limit is
+  // 60, minus one if it would have ended with a hyphen), it could have
+  // been truncated. If the target fullname is shorter than the original
+  // fullname (due to stripping the category), the last bit of the fullname
+  // is not recoverable
+  var couldHaveBeenTruncated =
+    fullname.length >= 59 && targetFullname.length < fullname.length;
+
+  // Find pages in the target branch matching this fullname
+  findPagesInSiteStartingWith(
+    targetBranch.id,
+    targetFullname,
+    function (fullnames) {
+      // If there is an exact match, a translation has been found
+      if (
+        fullnames.some(function (matchedFullname) {
+          // If the end of the fullname is possibly missing, check only
+          // that the matched fullname starts with the target.
+          // This is unlikely to produce a false positive because the
+          // fullnames involved are very long (~60 chars)
+          if (couldHaveBeenTruncated) {
+            return matchedFullname.indexOf(targetFullname) === 0;
+          }
+          // Otherwise, check for exact matches only
+          return matchedFullname === targetFullname;
+        })
+      ) {
+        addLink(
+          targetBranch.url + targetFullname,
+          targetBranch.name,
+          targetBranchLang
+        );
+      }
+    }
+  );
+}
 
 /**
  * In the given Wikidot site, searches for pages whose fullnames start with
